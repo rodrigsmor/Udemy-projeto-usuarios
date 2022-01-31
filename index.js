b8028e49f3bef0b86f19eeb9b8cbaadb6f224ec7
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-01-31 09:34:58 -0300

    Aula 4 - Cria variáveis e selecion pelos seletores CSS3

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var nome = document.querySelector('#exampleInputName');
-
-nome.value = 'Rodrigo Moreira'
-nome.style.color = 'blue'
+var name = document.querySelector('#exampleInputName');
+var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+var birth = document.querySelector('#exampleInputBirth');
+var country = document.querySelector('#exampleInputCountry');
+var email = document.querySelector('#exampleInputEmail');
+var password = document.querySelector('#exampleInputPassword');
+var photo = document.querySelector('#exampleInputFile');
+var admin = document.querySelector('#exampleInputAdmin');
