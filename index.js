224764320d15185a340b9518c07b81a9a56233e2
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-01-31 10:40:00 -0300

    Aula 8 - Adiciona usuário a tabela

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+const { data } = require("jquery");
+
 var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {};
 
+function addLine(dataUser) {
+    var tr = document.createElement('tr');
 
+    tr.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+
+    document.getElementById('table-users').appendChild(tr);
+}
 
 document.getElementById('form-user-create').addEventListener('submit', function(event) {
     event.preventDefault();
-    
     
     fields.forEach(function(field, index) {
         if(field.name === 'gender') {
@@ -16,6 +35,6 @@ document.getElementById('form-user-create').addEventListener('submit', function(
             user[field.name] = field.value;
         }
     });
-    
-    console.log(user);
+
+    addLine(user);
 });
