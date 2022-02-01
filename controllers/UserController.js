de3bb83caedf2595d3f51bf987c89f98777c1ea6
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-01 10:04:16 -0300

    Aula 18 - Implementa validação dos campos

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -61,8 +61,14 @@ class UserController {
 
     getValues() {
         let user = {};
+        let isValid = true;
         
         [...this.formEl.elements].forEach(function(field, index) {
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
+            
             if(field.name == 'gender') {
                 if(field.checked) {
                     user[field.name] = field.value;
@@ -74,6 +80,10 @@ class UserController {
             }
         });
         
+        if(isValid) {
+            return false;
+        }
+
         return new User(
             user.name,
             user.gender,
